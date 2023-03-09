d22cc0cb0c50002abd384155fd930413ce7d994f
fogfog2 <sj.fogfog@gmail.com>
2023-03-09 16:31:02 +0900

    colon_split update

---
+++ script/colon_test/train_mono.sh
@@ -0,0 +1,11 @@
+export PYTHONPATH="${PYTHONPATH}:/home/sj/src/DepthEstimation" 
+
+#data path (# Parent folder of "/images/sequenceNN" path  )
+DATA_PATH=/media/sj/data/datasets/colon_new2
+
+#log path 
+LOG_PATH1=/home/sj/tmp/mono_resnet_50epoch
+
+#run script
+python ../../depthestimation/train_dummy.py --data_path=$DATA_PATH --log_dir=$LOG_PATH1 --split=custom_dummy_new --train_monodepth --no_teacher --png --train_model=resnet --num_epochs=50
+
